c7121d8be6d197866da6a5b6b8da1dd45d447f27
Rui <rui.ferreira@codeforall.com>
2024-05-02 20:53:52 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'swiper/swiper-bundle.css'; // Import Swiper CSS
+import Swiper from 'swiper/bundle'; // Import Swiper JS
 
 // https://vitejs.dev/config/
 export default defineConfig({
